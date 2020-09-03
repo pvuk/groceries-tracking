a58c154aa274781ff51761e5c24c364235d8fd42
P. V. UdayKiran <pvuk1988@gmail.com>
2020-09-03 21:22:29 +0530

    Initial commit

---
+++ src/main/java/com/spring/mongodb/groceries/util/GroceriesTrackingUtil.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.spring.mongodb.groceries.util;
+
+/**
+ * @author P.V. UdayKiran
+ * @version 1, created on Thu 03-Sep-2020 21:22
+ */
+public class GroceriesTrackingUtil {
+
+}
