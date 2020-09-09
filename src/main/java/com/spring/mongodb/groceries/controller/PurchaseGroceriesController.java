ba8b15d0396f0b8851ca64a3496c9fc375e2d4d4
P. V. UdayKiran <pvuk1988@gmail.com>
2020-09-09 21:29:21 +0530

    Rest and Filename changes

--- src/main/java/com/spring/mongodb/groceries/controller/PurchaseGroceriesController.java
+++ src/main/java/com/spring/mongodb/groceries/controller/PurchaseGroceriesController.java
@@ -3,10 +3,15 @@
  */
 package com.spring.mongodb.groceries.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author P.V. UdayKiran
  * @version 1, created on Wed 02-Sep-2020 22:03
  */
+@RestController
+@RequestMapping(path = "/purchaseGroceries/custom")
 public class PurchaseGroceriesController {
 
 }
--- src/main/java/com/spring/mongodb/groceries/controller/PurchaseRemainderController.java
+++ src/main/java/com/spring/mongodb/groceries/controller/PurchaseRemainderController.java
@@ -1,9 +1,14 @@
 package com.spring.mongodb.groceries.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author P.V. UdayKiran
  * @version 1, created on Fri 04-Sep-2020 21:18
  */
+@RestController
+@RequestMapping(path = "purchaseRemainder/custom")
 public class PurchaseRemainderController {
-	
+
 }
